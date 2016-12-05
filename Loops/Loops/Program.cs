e09157c0e23410f92eb65f544b21c1b3d7bf4ad4
Abhi Dutta <abhid94@gmail.com>
2016-12-05 16:16:58 +1100

    Added initial projects
    Some C# projects that I have done with Dom to begin learning about C#

---
+++ Loops/Loops/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+
+namespace Loops
+{
+	class MainClass
+	{
+		public static void Main(string[] args)
+		{
+			//loopFunction();
+			//helloWorld();
+			//helloWorldArg(args[0]);
+			read();
+		}
+	
+
+		/// <summary>
+		/// prints out all the odd integers between 1 and 100
+		/// </summary>
+		public static void loopFunction()
+		{
+			int x = 0;
+
+			for (int i = 0; i <= 100; i++)
+			{
+
+				if (x % 2 != 0)
+				{
+					System.Console.WriteLine(x);
+				}
+				x++;
+
+			}
+		}
+
+
+		/// <summary>
+		///prints hello world
+		/// </summary>
+		public static void helloWorld()
+		{
+			System.Console.WriteLine("Hello World");
+		}
+
+		/// <summary>
+		///reads in an argument and prints it with "Hello" preceeding
+		/// </summary>
+		/// <param name="arg1">Arg1.</param>
+		public static void helloWorldArg(String arg1)
+		{
+
+			System.Console.WriteLine("Hello " + arg1);
+		}
+
+
+		/// <summary>
+		/// asks for name, then prints name with "Hello" preceeding
+		/// </summary>
+		public static void read()
+		{
+			System.Console.Write("Enter a Name: ");
+			var input = Console.ReadLine();
+
+			System.Console.WriteLine("Hello " + input);
+		}
+
+
+	}
+
+
+
+}
