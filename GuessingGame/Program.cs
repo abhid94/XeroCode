969f288f4d579b94f9ed3895d9ad80049107ed4d
Abhi Dutta <abhid94@gmail.com>
2016-12-06 16:54:14 +1100

    Removed the 0 condition in FizzBuzz, also added a starter project for the guessing game

---
+++ GuessingGame/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace GuessingGame
+{
+	//Write a simple game (console app) to create a random number between 0 and 100. 
+	//The program will ask you for the number and will ask you each turns the number. 
+	//If the number you select is > or < than the random number it will tell you this 
+	//and get you to enter the number again (while incrementing the number of turns). 
+	//Eventually when you enter the correct the number it will display the number of turns.
+
+	class MainClass
+	{
+		public static void Main(string[] args)
+		{
+			
+		}
+	}
+}
