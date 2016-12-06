3a7d9a94f6b14c31a5b8d02dd754abec0bdb2887
Abhi Dutta <abhid94@gmail.com>
2016-12-06 16:29:09 +1100

    Added condition for 0 in the fizzBuzz solution

--- FizzBuzz/Program.cs
+++ FizzBuzz/Program.cs
@@ -11,6 +11,11 @@ namespace FizzBuzz
 		{
 			for (int i = 0; i <= 100; i++)
 			{
+				if (i == 0)
+				{
+					System.Console.WriteLine(i);
+					continue;
+				}
 				if (i % 3 == 0 && i % 5 == 0)
 				{
 					System.Console.WriteLine("Fizz Buzz");
