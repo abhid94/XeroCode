9710532c2a8558f61f37001b8e6b582d1e3596a7
Abhi Dutta <abhid94@gmail.com>
2016-12-06 15:07:11 +1100

    Last commit failed - added again

---
+++ FizzBuzz/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace FizzBuzz
+
+//Build a fizz buzz program between the numbers 0 and 100 https://en.wikipedia.org/wiki/Fizz_buzz
+
+{
+	class MainClass
+
+
+	{
+		public static void Main(string[] args)
+		{
+			for (int i = 1; i <= 100; i++)
+			{
+				if (i % 3 == 0 && i % 5 == 0)
+				{
+					System.Console.WriteLine("Fizz Buzz");
+					continue;
+				}
+				if (i % 3 == 0)
+				{
+					System.Console.WriteLine("Fizz");
+					continue;
+				}
+				if (i % 5 == 0)
+				{
+					System.Console.WriteLine("Buzz");
+					continue;
+				}
+				System.Console.WriteLine(i);
+
+			}
+		}
+	}
+}
